9bb42745c60cff37fed75250c86e7c3e96b12919
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-03-20 10:32:05 +0100

    Add the packages for Moya and JGProgressHUD to the project definition

--- Pokedex/Project.swift
+++ Pokedex/Project.swift
@@ -5,5 +5,8 @@ import ProjectDescriptionHelpers
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
 let project = Project.app(name: "Pokedex",
+                          packages: [.package(url: "https://github.com/JonasGessner/JGProgressHUD", .upToNextMajor(from: "2.0.0")),
+                                     .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
+                                     .package(url: "https://github.com/antitypical/Result.git", from: "5.0.0")],
                           platform: .iOS,
                           additionalTargets: [])
