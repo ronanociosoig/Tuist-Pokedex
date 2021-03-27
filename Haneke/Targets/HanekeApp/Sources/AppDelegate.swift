dd1b2d0f6b48d630ced2cbab089a737d119f7ea2
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-03-27 22:22:07 +0100

    Implement the HanekeApp that displays an image that loads using the Haneke framework

--- Haneke/Targets/HanekeApp/Sources/AppDelegate.swift
+++ Haneke/Targets/HanekeApp/Sources/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
-import HanekeKit
-import HanekeUI
+import Haneke
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,14 +11,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .white
+        let viewController = makeViewController()
+        addImageView(to: viewController.view)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
-        HanekeKit.hello()
-        HanekeUI.hello()
-
         return true
     }
-
+    
+    func makeViewController() -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        return viewController
+    }
+    
+    func addImageView(to view: UIView) {
+        let imagePath = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/196.png"
+        let diameter: CGFloat = 128
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: diameter, height: diameter))
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo:view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: diameter),
+            imageView.heightAnchor.constraint(equalToConstant: diameter)
+        ])
+        
+        if let imageURL = URL(string: imagePath) {
+            imageView.hnk_setImage(from: imageURL)
+        }
+    }
 }
