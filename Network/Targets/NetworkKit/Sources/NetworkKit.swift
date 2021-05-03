fcc4c80a73baa3b1d299498e16e0a7faa218b605
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-05-03 23:21:20 +0200

    Add public to all the classes and properties that need it, copy over existing test, remove default generated class

--- Network/Targets/NetworkKit/Sources/NetworkKit.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public final class NetworkKit {
-    public static func hello() {
-        print("Hello, from your Kit framework")
-    }
-}
--- Network/Targets/NetworkKit/Tests/NetworkKitTests.swift
+++ Network/Targets/NetworkKit/Tests/NetworkKitTests.swift
@@ -1,8 +1,37 @@
-import Foundation
-import XCTest
+//
+//  NetworkKitTests.swift
+//  NetworkKitTests
+//
+//  Created by Ronan on 09/05/2019.
+//  Copyright © 2019 Sonomos. All rights reserved.
+//
 
-final class NetworkKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("NetworkKit", "NetworkKit")
+import XCTest
+import NetworkKit
+
+// swiftlint:disable all
+
+class NetworkKitTests: XCTestCase {
+    func testEndpointReturnsData() {
+        
+        let expectation = self.expectation(description: "No results in response data")
+        
+        let searchService = PokemonSearchService()
+        
+        searchService.search(identifier: 1) { (data, error) in
+            if let error = error {
+                print("Error: \(error)")
+                XCTFail("Error in the server call")
+                return
+            }
+            
+            if let data = data {
+                let length = data.count
+                print("We have some data.: \(length)")
+                expectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
     }
 }
