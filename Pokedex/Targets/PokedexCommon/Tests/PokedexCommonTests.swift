5eb3c0cc2bddaa2b729d40465659b361adaeeecf
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-05-23 00:39:52 +0200

    Add the generated files in PokedexCommon and define it as a local framework in the project manifest

---
+++ Pokedex/Targets/PokedexCommon/Tests/PokedexCommonTests.swift
@@ -0,0 +1,27 @@
+//
+//  PokedexCommonUnitTests.swift
+//  PokedexCommonExample
+//
+//  Created by .
+//  Copyright © 2021. All rights reserved.
+//
+
+@testable import PokedexCommon
+
+import XCTest
+
+class PokedexCommonTests: XCTestCase {
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+
+    func testPokedexCommon() {
+        // Given
+        // When
+        // Then
+    }
+}
