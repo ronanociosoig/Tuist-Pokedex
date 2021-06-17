d941ccf6b75a23a9a3e2acd170efcf9ac07c7fee
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-06-18 00:55:50 +0200

    Copy over AppController and Coordinator, and simplify

--- Backpack/Targets/Backpack/Sources/AppDelegate.swift
+++ Backpack/Targets/Backpack/Sources/AppDelegate.swift
@@ -1,22 +1,18 @@
 import UIKit
-import BackpackUI
+import PokedexCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    private let appController = AppController()
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .white
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        BackpackUI.hello()
-
+        
+        appController.start()
+        
         return true
     }
 }
