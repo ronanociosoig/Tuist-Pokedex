744121e5b78edd8bfe396dab938da2c399651b71
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-06-19 01:05:51 +0200

    Add a public init to the LocalPokemon struct

--- Pokedex/Targets/PokedexCommon/Sources/Model/LocalPokemon.swift
+++ Pokedex/Targets/PokedexCommon/Sources/Model/LocalPokemon.swift
@@ -18,4 +18,24 @@ public struct LocalPokemon: Codable {
     public let species: String
     public let baseExperience: Int
     public let types: [String]
+    
+    public init(name: String,
+                weight: Int,
+                height: Int,
+                order: Int,
+                spriteUrlString: String?,
+                date: Date,
+                species: String,
+                baseExperience: Int,
+                types: [String]) {
+        self.name = name
+        self.weight = weight
+        self.height = height
+        self.order = order
+        self.spriteUrlString = spriteUrlString
+        self.date = date
+        self.species = species
+        self.baseExperience = baseExperience
+        self.types = types
+    }
 }
