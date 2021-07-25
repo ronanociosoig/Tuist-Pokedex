df5bf33c859f869864a65e6299b01521e8b8860b
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-07-25 22:39:41 +0100

    Clean up

---
+++ Pokedex/Targets/PokedexCommon/Sources/Core/Updatable.swift
@@ -0,0 +1,14 @@
+//
+//  Updatable.swift
+//  PokedexCommon
+//
+//  Created by Ronan on 09/05/2019.
+//  Copyright © 2019 Sonomos. All rights reserved.
+//
+
+import Foundation
+
+public protocol Updatable {
+    func update()
+    func showError(message: String)
+}
