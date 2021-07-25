2c1e3a06ed3ad5c97e7eb6d1fb2a5512ab3d54ee
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-07-25 08:47:25 +0100

    Add Catch Home scene

---
+++ Catch/Targets/Catch/Sources/Scenes/Home Scene/HomeDataProvider.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDataProvider.swift
+//  Catch
+//
+//  Created by Ronan O Ciosig on 01/07/21.
+//  Copyright © 2021 Sonomos.com. All rights reserved.
+//
+
+import PokedexCommon
+
+protocol HomeDataProvider {
+
+}
+
+extension DataProvider: HomeDataProvider {
+ 
+}
