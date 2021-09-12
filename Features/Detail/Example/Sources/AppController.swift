068b9b621ad2f79099df6ce0ff9aae721e882b54
Ronan O'Ciosoig <ronan.ociosoig@wefoxgroup.com>
2021-09-12 17:26:32 +0200

    Add generated detail module

---
+++ Features/Detail/Example/Sources/AppController.swift
@@ -0,0 +1,35 @@
+//
+//  AppController.swift
+//  DetailExample
+//
+//  Created by Ronan on 12/09/2021.
+//  Copyright © 2021 Sonomos. All rights reserved.
+//
+
+import Foundation
+import Common
+
+protocol AppControlling {
+    func start()
+}
+
+class AppController: AppControlling {
+    var coordinator: Coordinating?
+
+    func start() {
+        let dataProvider = DataProvider()
+
+        if Configuration.uiTesting == true {
+            let storage = FileStorage()
+            storage.remove(AppData.pokemonFile, from: dataProvider.appData.directory())
+        }
+
+        dataProvider.start()
+
+        coordinator = Coordinator()
+        coordinator?.dataProvider = dataProvider
+        coordinator?.start()
+
+        dataProvider.notifier = coordinator as? Notifier
+    }
+}
---
+++ Features/Detail/Tests/DetailTests.swift
@@ -0,0 +1,28 @@
+//
+//  DetailTests.swift
+//  Detail
+//
+//  Created by Ronan on 12/09/2021.
+//  Copyright © 2021 Sonomos. All rights reserved.
+//
+
+@testable import Detail
+
+import XCTest
+
+class DetailTests: XCTestCase {
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+
+    func testDetail() {
+        // Given
+        // When
+        // Then
+    }
+}
+
