1df08005c3a6006caca05df7f5f84847ab61dc69
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2021-12-31 17:16:00 +0000

    Fix the swift lint step for the MacBook M1 processor as HomeBrew uses a different path for the command

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+if test -d "/opt/homebrew/bin/"; then
+  PATH="/opt/homebrew/bin/:${PATH}"
+fi
+
+export PATH
+
 SOURCES_PATH="Features/$1/Sources"
 
 if which swiftlint >/dev/null; then
@@ -7,4 +13,3 @@ swiftlint --path $SOURCES_PATH --config .swiftlint.yml --quiet
 else
 echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
-
